2b63ffcc42f2554201f0523363688b4b4b58c5ca
Pato Jofre <luis.jofre.g@gmail.com>
2016-10-18 00:32:08 -0300

    full refactor, now user can act as otpable

--- lib/otp/qr.rb
+++ lib/otp/qr.rb
@@ -2,43 +2,48 @@ require "otp/qr/version"
 require "rotp"
 require "google-qr"
 
-module OTP
-  class QR
+module ActsAsOTPable
 
-    def initialize(secret, issuer)
-      before_setup(issuer)
-      secret = secret || ROTP::Base32.random_base32
-      @otp = ROTP::TOTP.new(secret, issuer: issuer)
-    end
+  def otp_generate_secret
+    self.secret = ROTP::Base32.random_base32
+    self.save
+    self.secret
+  end
 
-    def generate_qr(email, size = "150x150", margin = 4)
-      before_generate(email, size, margin)
-      chart = GoogleQR.new(:data => @otp.provisioning_uri(email), :size => size, :margin => margin, :error_correction => "L")
-      chart.to_s
-    end
+  def otp_current_code
+    @otp = ROTP::TOTP.new(self.secret, issuer: ActsAsOTPable.config.issuer)
+    @otp.now
+  end
 
-    def current_code
-      @otp.now
+  def otp_qr_code
+    @otp = ROTP::TOTP.new(self.secret, issuer: ActsAsOTPable.config.issuer)
+    chart = GoogleQR.new(:data => @otp.provisioning_uri(self.email), :size => ActsAsOTPable.config.size, :margin => ActsAsOTPable.config.margin, :error_correction => "L")
+    chart.to_s
+  end
+
+  def otp_valid_code?(code, drift = 30)
+    @otp = ROTP::TOTP.new(self.secret, issuer: ActsAsOTPable.config.issuer)
+    @otp.verify_with_drift(code.to_s, 60, Time.now - drift)
+  end
+
+  def self.config
+    @config ||= Config.new
+  end
+
+  def self.configure
+    yield config
+  end
+
+  class Config
+
+    attr_accessor *%w(issuer size margin)
+
+    def initialize
+      @issuer        = nil
+      @size          = nil
+      @margin        = nil
     end
     
-    def validate_code(code, drift = nil)
-      drift = drift || 0
-      @otp.verify_with_drift(code.to_s, 60, Time.now - drift)
-    end
-
-    private
-
-    def before_setup(issuer)
-      raise "you should provide a issuer" and return if issuer == "" || issuer.nil?
-    end
-
-    def before_generate(email, size, margin)
-      size = size.split("x")
-      raise "invalid size" if size[0].to_i == 0 || size[1].to_i == 0
-      raise "invalid margin" if margin.to_i == 0
-      raise "you should provide a valid email" and return if email == "" || email.nil?
-      raise "first initialize the otp" and return if @otp.nil? 
-    end
-
   end
+
 end
